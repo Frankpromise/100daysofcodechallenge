d4e80dd3d616d0059ffb3909dd2f27630d7c3493
promise <promise@DESKTOP-5V7NG46.localdomain>
2022-05-01 23:37:32 +0200

    Band name generator

---
+++ day1.py
@@ -0,0 +1,4 @@
+city_name = input('What is the name of your city? \n')
+pet_name = input('What is the name of your pet? \n')
+Band_name = city_name + pet_name
+print (f'your band name is {Band_name}')
